80e0d8ae40459f66657f9d886268c7ad8ec62952
Dmitry <mezentsevdmitry51@gmail.com>
2018-05-27 03:11:14 +0500

    Добавил в проект документацию по установке.

--- gulpfile.js
+++ gulpfile.js
@@ -6,35 +6,65 @@ const path = require('path');
 
 
 
-const buildPath  = './ddelivery_woocommerce.zip';
-const moduleDir = './ddelivery_woocommerce';
+const buildDir  = './build';
+const moduleDir = './src';
+const docDir    = './doc';
 
 const moduleFiles = path.join(moduleDir, '**/*.*');
+const docFiles    = path.join(docDir, '*.*');
+
+const moduleFileName  = 'ddelivery_woocommerce_install.zip';
+const archiveFileName = 'ddelivery_woocommerce.zip';
 
 
 
 const $ = {
-    gulp:  require('gulp'),
-    zip:   require('gulp-zip'),
-    watch: require('gulp-watch'),
+    gulp:     require('gulp'),
+    zip:      require('gulp-zip'),
+    clean:    require('gulp-clean'),
+    watch:    require('gulp-watch'),
+    sequence: require('run-sequence'),
 };
 
 
 
+// Удаление старых файлов сборки
+$.gulp.task('_clean', () =>
+    $.gulp.src(path.join(buildDir, '*.*'), { read: false })
+        .pipe($.clean())
+);
+
 // Сборка модуля
 $.gulp.task('_buildModule', () =>
-    $.gulp.src(moduleFiles, { base: '.' })
-        .pipe($.zip(buildPath))
-        .pipe($.gulp.dest('.'))
+    $.gulp.src(moduleFiles, { base: moduleDir })
+        .pipe($.zip(moduleFileName))
+        .pipe($.gulp.dest(buildDir))
 );
+
+// Создание архива для выгрузки на сайт
+$.gulp.task('_createArchive', () => {
+    let src = [
+        path.join(buildDir, moduleFileName),
+        docFiles,
+        // exclude
+        path.join(`!${docDir}`, '~*'),
+        path.join(`!${docDir}`, '*.tmp'),
+    ];
+
+    return $.gulp.src(src)
+        .pipe($.zip(archiveFileName))
+        .pipe($.gulp.dest(buildDir))
+});
 
 // Мониторинг изменений и пересборка
 $.gulp.task('_watch', () =>
-    $.watch(moduleFiles, () => $.gulp.start('_buildModule'))
+    $.watch([moduleFiles, docFiles], () =>
+        $.sequence('_clean', '_buildModule', '_createArchive')
+    )
 );
 
 
 
 $.gulp.task('default', () =>
-    $.gulp.start('_buildModule', '_watch')
+    $.sequence('_clean', '_buildModule', '_createArchive', '_watch')
 );
