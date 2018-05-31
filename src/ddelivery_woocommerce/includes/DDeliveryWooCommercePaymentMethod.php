f390af5f7b88121e70a83d1944f28d36d513a736
Dmitry <mezentsevdmitry51@gmail.com>
2018-06-01 04:37:43 +0500

    -

--- src/ddelivery_woocommerce/includes/DDeliveryWooCommercePaymentMethod.php
+++ src/ddelivery_woocommerce/includes/DDeliveryWooCommercePaymentMethod.php
@@ -7,55 +7,53 @@ require_once 'DDeliveryWooCommerceBase.php';
  */
 function addDDeliveryPaymentMethod()
 {
-	if (!DDeliveryWooCommerceBase::checkWooCommerce()) return;
-	
-	
-	function _addDDeliveryPaymentMethod($methods)
-	{
-		$methods[DDeliveryWooCommerceBase::ID] = 'DDeliveryWooCommercePaymentMethod';
-		return $methods;
-	}
-	
-	
-	function _initDDeliveryPaymentMethod()
-	{
-		class DDeliveryWooCommercePaymentMethod extends WC_Payment_Gateway
-		{
-			const TITLE = 'Payment via DDelivery';
-			
-			public function __construct()
-			{
-				$this->id                 = DDeliveryWooCommerceBase::ID;
-				$this->method_title       = __(self::TITLE, DDeliveryWooCommerceBase::TEXT_DOMAIN);
-				$this->method_description = __('Payment via DDelivery Widget', DDeliveryWooCommerceBase::TEXT_DOMAIN);
-				$this->title              = $this->get_option('title');
-				$this->has_fields         = false;
-				
-				if (!$this->title)
-				{
-					$this->update_option('title', __(self::TITLE, DDeliveryWooCommerceBase::TEXT_DOMAIN));
-					$this->title = __(self::TITLE, DDeliveryWooCommerceBase::TEXT_DOMAIN);
-				}
-				
-				$this->init_form_fields();
-				$this->init_settings();
-				
-				add_action('woocommerce_update_options_payment_gateways_' . $this->id, [$this, 'process_admin_options']);
-			}
-			
-			public function init_form_fields()
-			{
-				$this->form_fields = [
-					'title' => [
-						'title' => __('Title', 'woocommerce'),
-						'type' => 'text',
-					],
-				];
-			}
-		}
-	}
-	
-	
-	add_filter('woocommerce_payment_gateways', '_addDDeliveryPaymentMethod');
-	add_action('plugins_loaded', '_initDDeliveryPaymentMethod');
+    if (!DDeliveryWooCommerceBase::checkWooCommerce()) return;
+
+
+    function _addDDeliveryPaymentMethod($methods)
+    {
+        $methods[DDeliveryWooCommerceBase::ID] = 'DDeliveryWooCommercePaymentMethod';
+        return $methods;
+    }
+
+
+    function _initDDeliveryPaymentMethod()
+    {
+        class DDeliveryWooCommercePaymentMethod extends WC_Payment_Gateway
+        {
+            public function __construct()
+            {
+                $this->id                 = DDeliveryWooCommerceBase::ID;
+                $this->method_title       = __('Payment via DDelivery', DDeliveryWooCommerceBase::TEXT_DOMAIN);
+                $this->method_description = __('Payment via DDelivery Widget', DDeliveryWooCommerceBase::TEXT_DOMAIN);
+                $this->title              = $this->get_option('title');
+                $this->has_fields         = false;
+
+                if (!$this->title)
+                {
+                    $this->update_option('title', __('Payment via DDelivery', DDeliveryWooCommerceBase::TEXT_DOMAIN));
+                    $this->title = __('Payment via DDelivery', DDeliveryWooCommerceBase::TEXT_DOMAIN);
+                }
+
+                $this->init_form_fields();
+                $this->init_settings();
+
+                add_action('woocommerce_update_options_payment_gateways_' . $this->id, [$this, 'process_admin_options']);
+            }
+
+            public function init_form_fields()
+            {
+                $this->form_fields = [
+                    'title' => [
+                        'title' => __('Title', 'woocommerce'),
+                        'type' => 'text',
+                    ],
+                ];
+            }
+        }
+    }
+
+
+    add_filter('woocommerce_payment_gateways', '_addDDeliveryPaymentMethod');
+    add_action('plugins_loaded', '_initDDeliveryPaymentMethod');
 }
